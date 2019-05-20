9b6085e239d87c9a74309133e2ecacf85add44fa
李四 <lisi@qq.com>
2019-05-20 23:32:44 +0800

    我添加了住方法里面的一句话

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("11111");
+        SpringApplication.run(DemoApplication.class, args
+        );
     }
 
 }
